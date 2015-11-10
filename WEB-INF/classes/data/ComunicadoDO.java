f6ce70197127f7baf980effcf46490deb32186f8
JulianaM465 <juliana.oliveira465@gmail.com>
2015-11-10 09:35:07 -0200

    Arrumei o ComunicadoDO

--- WEB-INF/classes/data/ComunicadoDO.java
+++ WEB-INF/classes/data/ComunicadoDO.java
@@ -11,4 +11,42 @@ package data;
  */
 public class ComunicadoDO {
     
-}
+     private int _idrem;
+  private int _iddest;
+  private String _comun;
+  private String _data;
+
+  public int getIdrem() {
+     return _idrem;
+  } // getId do remetente
+
+  public void setIdrem(int idr) {
+    _idrem = idr;
+  } // setId do remetente
+  
+  public int getIdDest() {
+     return _iddest;
+  } // getId do Destinatário
+
+  public void setIdDest(int idd) {
+    _iddest = idd;
+  } // setId do Destinatário
+
+  public String getComunicado() {
+    return _comun;
+  } // obterComunicado
+
+  public void setComunicado(String comunicado) {
+    _comun = comunicado;
+  } // setComunicado
+
+
+  public String getData() {
+    return _data;
+  } // getData
+
+  public void setData(String dat) {
+    _data = dat;
+  } // setData
+    
+} // Comunicado
